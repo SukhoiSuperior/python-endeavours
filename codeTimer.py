5087515c9814c09a2bef86d485829b9c7d45f6de
BBXY <122512417+Smol-Gremlin@users.noreply.github.com>
2023-01-12 14:16:55 +0800

    Update codeTimer.py
    Signed-off-by: BBXY <122512417+Smol-Gremlin@users.noreply.github.com>

--- codeTimer.py
+++ codeTimer.py
@@ -16,3 +16,4 @@ endingTime,endingTime2 = time.perf_counter_ns(), time.perf_counter_ns()
 print()
 print(f"|Start time: {(startingTime+startingTime2)/2}| Ending time: {(endingTime+endingTime2)/2}| Time Taken: {((endingTime-startingTime)+(endingTime2-startingTime2))/2}|Mode: Performance Counter [Nanoseconds]|")
 #Repeated twice due to inaccuracies in logging of time
+#I was here UwU
